2ddfbf8798de7460594c77d0c8bd82850cb0136d
farukalways <alishopon367@gmail.com>
2025-05-15 08:51:19 +0600

    I made the title, description, tag, dynamic inside the list component.

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import SearchBox from "./SearchBox";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 const TaskBoard = () => {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "I want to learn react such than can Treat it like my slave and make it do whatever I want to do",
+    tags: ["web", "react", "js"],
+    priority: "Low",
+    isFavorite: true,
+  };
+
+  const [tasks, setTasks] = useState([defaultTask]);
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
         <SearchBox />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction />
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
